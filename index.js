d59d54091af462b5b296ea24d2b95b45b46e4d9a
Kenneth Barinas <barinaskennethSilva>
2024-08-26 07:16:29 +0800

    08-26-2024

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const dashclose = document.querySelector('#dashclose');
-  const dashboard = document.querySelector('#dashboard');
-const opendash = document.querySelector('.opendash');
 
-  dashclose.addEventListener('click', function(event) {
-    dashboard.style.width = "0%";
-    dashclose.style.width = "0%";
-  });
-  opendash.addEventListener('click', function(event) {
-    dashboard.style.width = "60%";
-    dashclose.style.width = "40%";
-  });
-});
+function closeUp(){
+  document.getElementById('set1').style.animation = "skillsAnimation 2s infinite";
+}
+
+
 window.addEventListener('scroll', (event) => {
-  if (window.pageYOffset >= 5) {
-    dashboard.style.width = "0%";
-    dashclose.style.width = "0%";
+  console.log('Scrolled', window.pageYOffset); 
+
+  if (window.pageYOffset >= 10) {
+    document.getElementById('set1').style.display = "none";
   }
 });
 
